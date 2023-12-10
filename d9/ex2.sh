c2993eef429ad3ad7cf36f3119730ced865073f9
Valentin Grimaldi <valentin.grimaldi@outlook.com>
2023-12-10 09:53:47 +0100

    Add day 9

---
+++ d9/ex2.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+if [[ $# != 1 || ! -f "$1" ]] ; then
+  exit 1
+fi
+
+function extrapolate() {
+  line_arr=( $@ )
+  next_line_arr=()
+  for i in ${!line_arr[@]}; do
+    [[ $(( i + 1 )) == ${#line_arr[@]} ]] && break
+    next_line_arr+=( $(( ${line_arr[$i + 1]} - ${line_arr[$i]} )) )
+  done
+  next_line_arr_str="${next_line_arr[@]}"
+  next_line_arr_str=${next_line_arr_str// /}
+  if [[ $next_line_arr_str =~ ^0+$ ]]; then
+    echo 0
+  else
+    val=$(extrapolate ${next_line_arr[@]})
+    echo $(( ${next_line_arr[0]} - $val ))
+  fi
+}
+res=0
+
+readarray -t LINE_ARRAY < "$1"
+for line in "${LINE_ARRAY[@]}"; do
+  val=$(extrapolate $line)
+  line_arr=( $line )
+  (( res +=  ${line_arr[0]} - $val ))
+done
+
+
+echo "Result : ${res}"
